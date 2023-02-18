efe3117f3147a81eb2e7cc520a127453a0be4c3b
Shreyash Gadgil <shreyashgadgil007@gmail.com>
2023-02-19 01:32:22 +0530

    BFS,DFS,UCS,Beacon
    All solutions implemented

--- assignment/Assignment-1/search.py
+++ assignment/Assignment-1/search.py
@@ -11,22 +11,9 @@ Implement your search algorithsm here
 """
 
 import operator
-import numpy as np
-import heapq
 
 def depthFirstSearch(problem):
-  """
-  Your search algorithm needs to return a list of actions that reaches the goal
-  Strategy: Search the deepest nodes in the search tree first
-  """
-  "*** YOUR CODE HERE ***"
 
-def breadthFirstSearch(problem):
-  """
-  Your search algorithm needs to return a list of actions that reaches the goal
-  Strategy: Search the shallowest nodes in the search tree first.
-  """
-  "*** YOUR CODE HERE ***"
   print ("Start:", problem.getStartState())
   visited=[[False for i in range(20)]for i in range(16)]
   x=0
@@ -36,16 +23,51 @@ def breadthFirstSearch(problem):
       if len(fringe)==0:      
         print('No solution found')
         return []
-      print (len(fringe))
-      #print (fringe)
+      c_node=fringe.pop()
+      p=c_node[1]
+      act=c_node[2]
+      visited[c_node[0][0]][c_node[0][1]]=True
+      x+=1
+      if problem.isGoalState(c_node[0]):
+          print("Solution found")
+          print("no. of nodes traversed:",x)
+          print('cost:',act)
+          return c_node[1]    
+      else:
+          for state,action,cost in problem.getSuccessors(c_node[0]):
+            if visited[state[0]][state[1]]==False:
+              #x+=1
+              a=p+[action]
+              act1=act+int(cost)
+              c=[state,a,act1]
+              fringe.append(c)
+              visited[state[0]][state[1]]=True
+              
+ 
+
+def breadthFirstSearch(problem):
+  """
+  Your search algorithm needs to return a list of actions that reaches the goal
+  Strategy: Search the shallowest nodes in the search tree first.
+  """
+  "*** YOUR CODE HERE ***"
+  print ("Start:", problem.getStartState())
+  #visited=[[False for i in range(20)]for i in range(16)]
+  x=0
+  start=problem.getStartState()
+  closed=[]
+  fringe=[[start,[],0]]
+  while True:
+      if len(fringe)==0:      
+        print('No solution found')
+        return []
       c_node=fringe.pop(0)
       p=c_node[1]
       cos=c_node[2]
-      visited[c_node[0][0]][c_node[0][1]]=True
-     
+      #visited[c_node[0][0]][c_node[0][1]]=True
+      closed.append(c_node[0])
       
-     
-        
+      x+=1
       if problem.isGoalState(c_node[0]):
           print("Solution found")
           print("no. of nodes traversed:",x)
@@ -53,19 +75,14 @@ def breadthFirstSearch(problem):
           return c_node[1]    
       else:
           for state,action,cost in problem.getSuccessors(c_node[0]):
-            if visited[state[0]][state[1]]==False:
-              x+=1
+            if state not in closed:
+              #x+=1
               a=p+[action]
               cos1=cos+int(cost)
               c=[state,a,cos1]
-              flag_do_not_append = False
-              for node in fringe:
-                  if node[0] == c[0]:
-                      flag_do_not_append = True
-                      
-              if flag_do_not_append == False:
-                  fringe.append(c)
-
+              fringe.append(c)
+              closed.append(state)
+  
 def uniformCostSearch(problem):
     print ("Start:", problem.getStartState())
     visited_val=[[0 for i in range(22)] for i in range(18)]
@@ -73,13 +90,12 @@ def uniformCostSearch(problem):
     x=0
     start=problem.getStartState()
     fringe=[[start,[],0]]
-    closed=[]
     while True:
         if len(fringe)==0:      
           print('No solution found')
           return []
         fringe.sort(key = lambda x: x[2])
-
+        x+=1
         c_node=fringe.pop(0)
         p=c_node[1]
         cos=c_node[2]
@@ -93,7 +109,7 @@ def uniformCostSearch(problem):
             return c_node[1]    
         else:
             for state,action,cost in problem.getSuccessors(c_node[0]):
-                x+=1
+                #x+=1
                 a=p+[action]
                 cos1=cos+int(cost)
                 c=[state,a,cos1]
@@ -101,8 +117,4 @@ def uniformCostSearch(problem):
                 if visited[state[0]][state[1]]==False:
                     fringe.append(c)
                     visited_val[state[0]][state[1]]=cos1
-                    visited[state[0]][state[1]]=True
-                    
-
-
-  
+                    visited[state[0]][state[1]]=True  
